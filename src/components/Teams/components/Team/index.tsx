c500dc9f9105b477c243208b9f0c42404bde6968
theedgy <the.edgy@gmail.com>
2020-04-16 12:12:06 -0200

    Fix env

--- src/components/Teams/components/Team/index.tsx
+++ src/components/Teams/components/Team/index.tsx
@@ -1,33 +1,35 @@
-import React, {useContext} from 'react';
-import {AppContext, InterfaceTeam} from "../../../../store";
+import React, { useContext } from "react";
+import { AppContext, InterfaceTeam } from "../../../../store";
+import { setCurrentTeam } from "../../../../store/current/actions";
 
-import './index.scss';
-import {setCurrentTeam} from "../../../../store/current/actions";
-
+import "./index.scss";
 
 interface InterfaceTeamComponent {
-    team: InterfaceTeam,
-    current?: boolean,
+  team: InterfaceTeam;
+  current?: boolean;
 }
 
-export const Team: React.FC<InterfaceTeamComponent> = ({team, current = false}) => {
+export const Team: React.FC<InterfaceTeamComponent> = ({
+  team,
+  current = false
+}) => {
+  const { dispatch } = useContext(AppContext);
+  const onTeamSelect = (id: number) => {
+    dispatch && dispatch(setCurrentTeam(id));
+  };
 
-    const {dispatch} = useContext(AppContext);
-    const onTeamSelect = (id: number) => {
-        dispatch && dispatch(setCurrentTeam(id));
-    };
+  return (
+    <p className="Team">
+      <img src={team.crestUrl} alt={`#${team.shortName}`} />
 
-    return (
-        <p className="Team">
-            <img src={team.crestUrl}
-                 alt={`#${team.shortName}`} />
-
-            <a className={current ? 'active' : 'Team-link'}
-               href={`#${team.shortName}`}
-               onClick={() => onTeamSelect(team.id)}>
-                {`${team.name}`}
-            </a>
-        </p>
-    );
+      <button
+        type="button"
+        className={current ? "active" : "Team-link"}
+        name={`#${team.shortName}`}
+        onClick={() => onTeamSelect(team.id)}
+      >
+        {`${team.name}`}
+      </button>
+    </p>
+  );
 };
-
--- src/services/apiConnection/index.ts
+++ src/services/apiConnection/index.ts
@@ -1,15 +1,14 @@
-import {paramStringify} from '../paramsStringify';
+import { paramStringify } from "../paramsStringify";
 
-export const apiConnection = async (endpoint = '', data = null) => {
-    const url = data
-        ? `/${endpoint}?${paramStringify(data)}`
-        : `/${endpoint}`;
+export const apiConnection = async (endpoint = "", data = null) => {
+  const url = data
+    ? `https://api.football-data.org/v2/${endpoint}?${paramStringify(data)}`
+    : `https://api.football-data.org/v2/${endpoint}`;
 
-    return await fetch(url, {
-        method: 'GET',
-        headers: {
-            'X-Auth-Token': process.env.REACT_APP_API_KEY || '',
-        },
-    });
+  return await fetch(url, {
+    method: "GET",
+    headers: {
+      "X-Auth-Token": process.env.REACT_APP_API_KEY || ""
+    }
+  });
 };
-
